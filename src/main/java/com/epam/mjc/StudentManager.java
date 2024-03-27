8937e2644fe192b105dddee957b092cd7b91d622
vhorbatiuk <vhorbatiuk@binariks.com>
2024-03-27 18:03:33 +0200

    fix tests 3

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,14 +1,11 @@
 package com.epam.mjc;
 
-
-import java.util.Arrays;
-
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) throws StudentNotFoundException{
-    Student student = null;
+    Student student;
     if (IDs.length>studentID) {
       student = Student.getValueOf(studentID);
     }else {
