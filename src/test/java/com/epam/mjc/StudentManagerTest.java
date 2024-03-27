d14a370f6f84e27bcbfa14c488570c6e9bf1f05c
vhorbatiuk <vhorbatiuk@binariks.com>
2024-03-27 17:58:26 +0200

    fix tests

--- src/test/java/com/epam/mjc/StudentManagerTest.java
+++ src/test/java/com/epam/mjc/StudentManagerTest.java
@@ -11,27 +11,25 @@ public class StudentManagerTest {
     StudentManager manager = new StudentManager();
 
     @Test(expected = IllegalArgumentException.class)
-    public void findNotValid() throws IllegalArgumentException {
-
+    public void findNotValid() throws StudentNotFoundException {
         try {
             manager.find(1000);
         } catch (StudentNotFoundException e) {
             throw new IllegalArgumentException(e);
         }
-
     }
 
     @Test
-    public void findValidStudent() throws StudentNotFoundException {
+    public void findValidStudent() {
         try {
             assertNotNull(manager.find(1));
-        } catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException | StudentNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     @Test
-    public void testExceptionMessage() throws StudentNotFoundException {
+    public void testExceptionMessage()  {
         try {
             assertNotNull(manager.find(1000));
         } catch (StudentNotFoundException e) {
@@ -40,17 +38,21 @@ public class StudentManagerTest {
     }
 
     @Test
-    public void testIDsNotChangedV1() throws StudentNotFoundException {
+    public void testIDsNotChangedV1() {
         try {
             assertNull("Student enum should be have only 10 values.", manager.find(11));
         } catch (StudentNotFoundException e) {
-            new StudentNotFoundException("Student enum should be have only 10 values.");
+
         }
     }
 
     @Test
-    public void testIDsNotChangedV2() throws StudentNotFoundException {
-        assertEquals(Student.ARTUR, manager.find(Arrays.stream(Student.values()).map(Student::getId).max(Long::compare).get()));
+    public void testIDsNotChangedV2() {
+        try {
+            assertEquals(Student.ARTUR, manager.find(Arrays.stream(Student.values()).map(Student::getId).max(Long::compare).get()));
+        } catch (StudentNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
